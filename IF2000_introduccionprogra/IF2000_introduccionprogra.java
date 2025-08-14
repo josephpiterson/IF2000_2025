adc74e80d1080fb3065ae91a88afba9809aa43cd
unknown <josephpiterson022@gmail.com>
2025-08-14 11:29:30 -0600

    Mi Primer Commmit en Java

---
+++ IF2000_introduccionprogra/IF2000_introduccionprogra.java
@@ -0,0 +1,32 @@
+package IF2000_introduccionprogra;
+
+public class IF2000_introduccionprogra {
+   public static void main(String[] args) {
+    String name="Joseph";
+    int age=40;
+    /*tipos de datos numericos
+     * int,long,short,float,double,numeric,money,decimal
+     * 
+     * 
+     mandejo de fechas
+     date
+
+     cadenas de caracteres:
+     string
+
+     odjetos
+     odject
+
+     data structures
+     array or vectors: int[]x;
+                       int[][]y;
+    
+    true/false
+    boolean
+    */
+    System.out.println("Hola mundo");
+    System.out.println("Nombre:"+name);
+    System.out.println("Edad:"+age);
+
+   } 
+}

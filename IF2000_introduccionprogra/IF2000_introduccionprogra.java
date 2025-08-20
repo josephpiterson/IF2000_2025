ee1e46b6a048d6a341fcd7081cb75210dfc3652d
unknown <josephpiterson022@gmail.com>
2025-08-19 20:56:50 -0600

    segundo

--- IF2000_introduccionprogra/IF2000_introduccionprogra.java
+++ IF2000_introduccionprogra/IF2000_introduccionprogra.java
@@ -1,5 +1,7 @@
 package IF2000_introduccionprogra;
 
+import domain.Person;
+
 public class IF2000_introduccionprogra {
    public static void main(String[] args) {
     String name="Joseph";
@@ -27,6 +29,8 @@ public class IF2000_introduccionprogra {
     System.out.println("Hola mundo");
     System.out.println("Nombre:"+name);
     System.out.println("Edad:"+age);
+     Person person = new Person("John", "Doe", "123456789", "555-1234", 30);
+    System.out.println(person.toString());
 
    } 
 }

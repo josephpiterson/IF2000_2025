853f8c891e65fe52ba8384059cc10bf9b6da8669
josephpiterson <josephpiterson022@gmail.com>
2025-08-23 10:56:12 -0600

    Update Ejercicio_semana2_PracticaEnClase.java

--- logic/Ejercicio_semana2_PracticaEnClase.java
+++ logic/Ejercicio_semana2_PracticaEnClase.java
@@ -1,4 +1,15 @@
 package logic;
 public class Ejercicio_semana2_PracticaEnClase {
-    
+    public void Quizz_piramide_astericos(int n){
+        for(int i=1; i<=n; i++){
+            for (int j=1; j<=(n-i); j++){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=(2*i-1); k++){
+                System.out.print("*");
+            }
+        }
+        System.out.println("");
+
+    }
 }

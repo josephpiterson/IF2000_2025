877354831364ebb1a342b25c73d5ef47268ca849
josephpiterson <josephpiterson022@gmail.com>
2025-08-23 20:21:39 -0600

    Lab

--- IF2000_introduccionprogra/IF2000_introduccionprogra.java
+++ IF2000_introduccionprogra/IF2000_introduccionprogra.java
@@ -2,6 +2,8 @@ package IF2000_introduccionprogra;
 
 import domain.Person;
 import logic.Ejercicio_semana2_PracticaEnClase;
+import logic.lab1_ejercicios;
+
 public class IF2000_introduccionprogra {
    public static void main(String[] args) {
     String name="Joseph";
@@ -38,8 +40,19 @@ public class IF2000_introduccionprogra {
       ejercicio.ejercicioB(10);
       ejercicio.ejercicioc(10);
       ejercicio.conteo_Ascendente(10);
-      ejercicio.conteo_descendente(10);*/
-      ejercicio.mostrarTableroDamas();
+      ejercicio.conteo_descendente(10);
+      ejercicio.mostrarTableroDamas();*/
+      lab1_ejercicios lab1 = new lab1_ejercicios();
+      //lab1.Ejercio_1();
+      //lab1.Ejecicio_2();
+      //lab1.Ejercicio_3();
+      //lab1.Ejercicio_4();
+      //lab1.Ejercicio_5();
+      //lab1.Ejercicio_6();
+      //lab1.Ejercicio_7();
+      //lab1.Ejercicio_8();
+      //lab1.Ejercicio_9();
+      lab1.Ejercicio_10();
 
    } 
 }

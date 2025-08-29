a6989c6b87df3f99fdb9657faf8d54bff462f461
josephpiterson <josephpiterson022@gmail.com>
2025-08-28 20:23:19 -0600

    Update Lab2_ejercicios.java

--- logic/Lab2_ejercicios.java
+++ logic/Lab2_ejercicios.java
@@ -2,9 +2,7 @@ package logic;
 
 public class Lab2_ejercicios {
 
-    public lab2_ejercicios(){
-
-    }
+    public Lab2_ejercicios(){}
     public void ejercicio1(int n){
         // Realizar la suma de todos los números pares entre 2 y 1000.
         n=0;

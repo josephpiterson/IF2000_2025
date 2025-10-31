346bb6816ff30dde76ea6c65143f0470485f387b
josephpiterson <josephpiterson022@gmail.com>
2025-10-30 19:47:47 -0600

    Quizz

---
+++ logic/Quizz30-10-25/Person.java
@@ -0,0 +1,15 @@
+package logic.Quizz30-10-25;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Person {
+    private final String nombre;
+    private final String id; 
+    private final int edad;
+    private final String correo;
+    private final String telefono;
+    private final LocalDateTime fechaRegistro;
+    
+
+}

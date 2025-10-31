1fd58cdf54c3b5da8d37f1af79f306cda4df9342
josephpiterson <josephpiterson022@gmail.com>
2025-10-30 21:25:17 -0600

    Quizz

--- logic/Quizz30-10-25/Person.java
+++ logic/Quizz30-10-25/Person.java
@@ -10,6 +10,48 @@ public class Person {
     private final String correo;
     private final String telefono;
     private final LocalDateTime fechaRegistro;
+    private static final DateTimeFormatter FORMATO=DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    public Person(String nombre, String id, int edad, String correo, String telefono) {
+        this.nombre = nombre;
+        this.id = id;
+        this.edad = edad;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.fechaRegistro = LocalDateTime.now();
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public String getId(){
+        return id;
+    }
+    public int getEdad(){
+        return edad;
+    }
+    public String getCorreo(){
+        return correo;
+    }
+    public String getTelefono(){
+        return telefono;
+    }
+    public String getFechaRegistro(){
+        return fechaRegistro.format(FORMATO);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "nombre='" + nombre + '\'' +
+                ", id='" + id + '\'' +
+                ", edad=" + edad +
+                ", correo='" + correo + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", fechaRegistro=" + fechaRegistro.format(FORMATO) +
+                '}';
+    }
+
+
     
 
 }
